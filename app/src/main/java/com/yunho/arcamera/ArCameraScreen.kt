aea7d30808be28b24c0fb293a361aa01c1e5e86c
lyh990517 <lyh990517@naver.com>
2024-12-28 14:15:01 +0900

    screen separated

---
+++ app/src/main/java/com/yunho/arcamera/ArCameraScreen.kt
@@ -0,0 +1,142 @@
+package com.yunho.arcamera
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.google.android.filament.Engine
+import com.google.ar.core.Anchor
+import com.google.ar.core.Config
+import com.google.ar.core.Frame
+import com.google.ar.core.Plane
+import io.github.sceneview.ar.ARScene
+import io.github.sceneview.ar.arcore.createAnchorOrNull
+import io.github.sceneview.ar.arcore.getUpdatedPlanes
+import io.github.sceneview.ar.arcore.isValid
+import io.github.sceneview.ar.node.AnchorNode
+import io.github.sceneview.ar.rememberARCameraNode
+import io.github.sceneview.loaders.MaterialLoader
+import io.github.sceneview.loaders.ModelLoader
+import io.github.sceneview.node.CubeNode
+import io.github.sceneview.node.ModelNode
+import io.github.sceneview.rememberCollisionSystem
+import io.github.sceneview.rememberEngine
+import io.github.sceneview.rememberMaterialLoader
+import io.github.sceneview.rememberModelLoader
+import io.github.sceneview.rememberNodes
+import io.github.sceneview.rememberOnGestureListener
+import io.github.sceneview.rememberView
+
+@Composable
+fun ArCameraScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        val engine = rememberEngine()
+        val modelLoader = rememberModelLoader(engine)
+        val materialLoader = rememberMaterialLoader(engine)
+        val cameraNode = rememberARCameraNode(engine)
+        val childNodes = rememberNodes()
+        val view = rememberView(engine)
+        val collisionSystem = rememberCollisionSystem(view)
+
+        var frame by remember { mutableStateOf<Frame?>(null) }
+        ARScene(
+            modifier = Modifier.fillMaxSize(),
+            childNodes = childNodes,
+            engine = engine,
+            view = view,
+            modelLoader = modelLoader,
+            planeRenderer = false,
+            collisionSystem = collisionSystem,
+            sessionConfiguration = { session, config ->
+                config.depthMode =
+                    when (session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)) {
+                        true -> Config.DepthMode.AUTOMATIC
+                        else -> Config.DepthMode.DISABLED
+                    }
+                config.lightEstimationMode =
+                    Config.LightEstimationMode.ENVIRONMENTAL_HDR
+            },
+            cameraNode = cameraNode,
+            onSessionUpdated = { session, updatedFrame ->
+                frame = updatedFrame
+
+                if (childNodes.isEmpty()) {
+                    updatedFrame.getUpdatedPlanes()
+                        .firstOrNull { it.type == Plane.Type.HORIZONTAL_UPWARD_FACING }
+                        ?.let { it.createAnchorOrNull(it.centerPose) }?.let { anchor ->
+                            childNodes += createAnchorNode(
+                                engine = engine,
+                                modelLoader = modelLoader,
+                                materialLoader = materialLoader,
+                                anchor = anchor
+                            )
+                        }
+                }
+            },
+            onGestureListener = rememberOnGestureListener(
+                onSingleTapConfirmed = { motionEvent, node ->
+                    if (node == null) {
+                        val hitResults = frame?.hitTest(motionEvent.x, motionEvent.y)
+                        hitResults?.firstOrNull {
+                            it.isValid(
+                                depthPoint = false,
+                                point = false
+                            )
+                        }?.createAnchorOrNull()
+                            ?.let { anchor ->
+                                childNodes += createAnchorNode(
+                                    engine = engine,
+                                    modelLoader = modelLoader,
+                                    materialLoader = materialLoader,
+                                    anchor = anchor
+                                )
+                            }
+                    }
+                })
+        )
+    }
+}
+
+fun createAnchorNode(
+    engine: Engine,
+    modelLoader: ModelLoader,
+    materialLoader: MaterialLoader,
+    anchor: Anchor,
+): AnchorNode {
+    val anchorNode = AnchorNode(engine = engine, anchor = anchor)
+    val modelNode = ModelNode(
+        modelInstance = modelLoader.createModelInstance("models/halloween.glb"),
+        scaleToUnits = 0.01f,
+    ).apply {
+        isEditable = true
+        editableScaleRange = 0.2f..0.75f
+    }
+    val boundingBoxNode = CubeNode(
+        engine,
+        size = modelNode.extents,
+        center = modelNode.center,
+        materialInstance = materialLoader.createColorInstance(Color.White.copy(alpha = 0.5f))
+    ).apply {
+        isVisible = false
+    }
+    modelNode.addChildNode(boundingBoxNode)
+    anchorNode.addChildNode(modelNode)
+
+    listOf(modelNode, anchorNode).forEach {
+        it.onEditingChanged = { editingTransforms ->
+            boundingBoxNode.isVisible = editingTransforms.isNotEmpty()
+        }
+    }
+    return anchorNode
+}
