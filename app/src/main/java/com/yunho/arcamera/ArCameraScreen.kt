e6ce4eb4fb3f6f8bf1156e9136e709e2101afd73
lyh990517 <lyh990517@naver.com>
2024-12-28 17:08:15 +0900

    rotation

--- app/src/main/java/com/yunho/arcamera/ArCameraScreen.kt
+++ app/src/main/java/com/yunho/arcamera/ArCameraScreen.kt
@@ -1,14 +1,22 @@
 package com.yunho.arcamera
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.google.android.filament.Engine
 import com.google.ar.core.Anchor
 import com.google.ar.core.Config
@@ -20,6 +28,7 @@ import io.github.sceneview.ar.node.AnchorNode
 import io.github.sceneview.ar.rememberARCameraNode
 import io.github.sceneview.loaders.ModelLoader
 import io.github.sceneview.math.Position
+import io.github.sceneview.math.Rotation
 import io.github.sceneview.math.Scale
 import io.github.sceneview.node.ModelNode
 import io.github.sceneview.rememberCollisionSystem
@@ -28,6 +37,10 @@ import io.github.sceneview.rememberModelLoader
 import io.github.sceneview.rememberNodes
 import io.github.sceneview.rememberOnGestureListener
 import io.github.sceneview.rememberView
+
+enum class RotationAxis {
+    X, Y, Z
+}
 
 @Composable
 fun ArCameraScreen() {
@@ -42,6 +55,8 @@ fun ArCameraScreen() {
         val collisionSystem = rememberCollisionSystem(view)
         var modelScale by remember { mutableStateOf(0.5f) }
         var currentAnchor by remember { mutableStateOf<Anchor?>(null) }
+        var rotationMode by remember { mutableStateOf(RotationAxis.X) }
+        var rotationDegree by remember { mutableStateOf(Rotation(1f, 1f, 1f)) }
         var frame by remember { mutableStateOf<Frame?>(null) }
 
         ARScene(
@@ -73,6 +88,8 @@ fun ArCameraScreen() {
                                 pose.tx(), pose.ty(), pose.tz()
                             )
                         }
+
+                        rotation = rotationDegree
                     }
                 }
                 childNodes.clear()
@@ -116,8 +133,61 @@ fun ArCameraScreen() {
                 onScale = { scaleGestureDetector, motionEvent, node ->
                     val scaleFactor = scaleGestureDetector.scaleFactor
                     modelScale *= scaleFactor
-                })
+                },
+                onRotate = { r, m, n ->
+                    val rotation = r.currentAngle
+
+                    rotationDegree = when (rotationMode) {
+                        RotationAxis.X -> {
+                            Rotation(
+                                rotationDegree.x * rotation,
+                                rotationDegree.y,
+                                rotationDegree.z
+                            )
+                        }
+
+                        RotationAxis.Y -> {
+                            Rotation(
+                                rotationDegree.x,
+                                rotationDegree.y * rotation,
+                                rotationDegree.z
+                            )
+                        }
+
+                        RotationAxis.Z -> {
+                            Rotation(
+                                rotationDegree.x,
+                                rotationDegree.y,
+                                rotationDegree.z * rotation
+                            )
+                        }
+                    }
+                }
+            )
         )
+        Row(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .padding(bottom = 100.dp),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(onClick = {
+                rotationMode = RotationAxis.X
+            }) {
+                Text("X")
+            }
+            Button(onClick = {
+                rotationMode = RotationAxis.Y
+            }) {
+                Text("Y")
+            }
+            Button(onClick = {
+                rotationMode = RotationAxis.Z
+            }) {
+                Text("Z")
+            }
+        }
     }
 }
 
