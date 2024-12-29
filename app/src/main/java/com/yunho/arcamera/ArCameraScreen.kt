d55322bfa5fd259064c13a348e894dfd740e1ff4
lyh990517 <lyh990517@naver.com>
2024-12-29 13:08:07 +0900

    trim code

--- app/src/main/java/com/yunho/arcamera/ArCameraScreen.kt
+++ app/src/main/java/com/yunho/arcamera/ArCameraScreen.kt
@@ -4,10 +4,8 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.view.PixelCopy
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,21 +13,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
 import com.google.android.filament.Engine
 import com.google.ar.core.Anchor
@@ -51,6 +47,7 @@ import io.github.sceneview.rememberModelLoader
 import io.github.sceneview.rememberNodes
 import io.github.sceneview.rememberOnGestureListener
 import io.github.sceneview.rememberView
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.first
@@ -68,11 +65,12 @@ fun ArCameraScreen() {
         val childNodes = rememberNodes()
         val view = rememberView(engine)
         val collisionSystem = rememberCollisionSystem(view)
-        var modelScale by remember { mutableStateOf(0.5f) }
+        var modelScale by remember { mutableFloatStateOf(0.5f) }
         var currentAnchor by remember { mutableStateOf<Anchor?>(null) }
         var frame by remember { mutableStateOf<Frame?>(null) }
-        val captureBitmapState = remember { mutableStateOf<Bitmap?>(null) }
+        val captureResult = remember { mutableStateOf<Bitmap?>(null) }
         var arSceneView by remember { mutableStateOf<ARSceneView?>(null) }
+
         ARScene(
             modifier = Modifier
                 .fillMaxSize(),
@@ -112,12 +110,9 @@ fun ArCameraScreen() {
                 onSingleTapUp = { motionEvent, node ->
                     if (node == null) {
                         val hitResults = frame?.hitTest(motionEvent.x, motionEvent.y)
-                        hitResults?.firstOrNull {
-                            it.isValid(
-                                depthPoint = false,
-                                point = false
-                            )
-                        }?.createAnchorOrNull()
+                        hitResults
+                            ?.firstOrNull()
+                            ?.createAnchorOrNull()
                             ?.let { anchor ->
                                 childNodes.clear()
 
@@ -133,12 +128,9 @@ fun ArCameraScreen() {
                 onMove = { d, m, n ->
                     val hitResults = frame?.hitTest(m.x, m.y)
 
-                    hitResults?.firstOrNull {
-                        it.isValid(
-                            depthPoint = false,
-                            point = false
-                        )
-                    }?.createAnchorOrNull()
+                    hitResults
+                        ?.firstOrNull()
+                        ?.createAnchorOrNull()
                         ?.let { anchor ->
                             currentAnchor = anchor
                         }
@@ -152,47 +144,68 @@ fun ArCameraScreen() {
                 arSceneView = this
             }
         )
-        captureBitmapState.value?.let { bitmap ->
+        Menu(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            onClear = {
+                childNodes.clear()
+                currentAnchor = null
+                modelScale = 0.5f
+            },
+            onCapture = {
+                arSceneView?.let {
+                    scope.launch(Dispatchers.IO) {
+                        val byteArray = handleSnapshot(it).first()
+                        val bitmap = byteArrayToBitmap(byteArray)
+                        captureResult.value = bitmap
+                    }
+                }
+            }
+        )
+        CaptureResult(captureResult)
+    }
+}
+
+@Composable
+private fun Menu(
+    modifier: Modifier = Modifier,
+    onClear: () -> Unit,
+    onCapture: () -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = 100.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(onClick = {
+                onClear()
+            }) {
+                Text("Reset")
+            }
+            Button(onClick = {
+                onCapture()
+            }) {
+                Text("Capture")
+            }
+        }
+    }
+}
+
+@Composable
+private fun CaptureResult(captureBitmapState: MutableState<Bitmap?>) {
+    captureBitmapState.value?.let { bitmap ->
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
             Image(
                 bitmap = bitmap.asImageBitmap(),
                 contentDescription = null,
                 modifier = Modifier
-                    .size(200.dp)
-                    .background(Color.White)
-                    .align(Alignment.TopCenter)
+                    .fillMaxSize()
             )
-        }
-        Column(
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .fillMaxWidth()
-                .padding(bottom = 100.dp)
-        ) {
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                Button(onClick = {
-                    childNodes.clear()
-                    currentAnchor = null
-                    modelScale = 0.5f
-                }) {
-                    Text("Reset")
-                }
-                Button(onClick = {
-                    arSceneView?.let {
-                        scope.launch {
-                            val byteArray = handleSnapshot(it).first()
-                            val bitmap = byteArrayToBitmap(byteArray)
-                            Log.e("123", "$bitmap")
-                            captureBitmapState.value = bitmap
-                        }
-                    }
-                }) {
-                    Text("Capture")
-                }
-            }
         }
     }
 }
@@ -240,7 +253,7 @@ fun createAnchorNode(
     val anchorNode = AnchorNode(engine = engine, anchor = anchor)
     val modelNode = ModelNode(
         modelInstance = modelLoader.createModelInstance("models/halloween.glb"),
-        scaleToUnits = 0.01f,
+        scaleToUnits = 0.05f,
         centerOrigin = Position(pose.tx(), pose.qy(), pose.tz())
     ).apply {
         isEditable = true
